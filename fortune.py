59a1cf17ea5cfeb6507a08f4ef9387d8f54f0f49
Samudra <samudramitra2017@gmail.com>
2025-04-17 19:40:05 +0530

    added stressed mood

--- fortune.py
+++ fortune.py
@@ -5,7 +5,7 @@ def main():
     admission_number = "21JE0815" 
 
     print(f"\nWelcome to {fname} {lname}'s Fortune Teller ({admission_number})\n")
-    mood = input("How are you feeling today? (happy/sad/neutral): ").strip().lower()
+    mood = input("How are you feeling today? (happy/sad/neutral/stressed): ").strip().lower()
 
     fortunes = {
         "happy": [
@@ -19,6 +19,10 @@ def main():
         "neutral": [
             "Still waters run deep. Something surprising is coming.",
             "No chaos = more clarity. Use this moment."
+        ],
+        "stressed":[
+            "Streesing won't help at all, keep calm",
+            "Stressing is the mother of all problems, keep calm"
         ]
     }
 
